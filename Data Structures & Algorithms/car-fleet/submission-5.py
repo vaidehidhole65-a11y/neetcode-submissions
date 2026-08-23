3e3d5b6657cae574e4e03ce004505cb3276a8d85
vaidehi dhole <vaidehidhole65@gmail.com>
2026-08-23 19:35:23 +0530

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def carFleet(self, target, position, speed):
+
+        cars = list(zip(position, speed)) #zip corresponding value ko pair krta he
+        cars.sort(reverse=True)
+
+        stack = []
+
+        for pos, spd in cars:
+            time = (target - pos) / spd
+
+            if not stack or time > stack[-1]:
+                stack.append(time)
+
+        return len(stack)
+        #time= target−position[i]​/speed[i] 
+        #append = andar , pop = bahar
